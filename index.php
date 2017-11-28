0bb7b37303f6f17f079dcee173db05c4ccd38c1b
Tal Shemesh <unlock852@gmail.com>
2017-11-28 22:41:54 +0200

    dd
    dd

--- index.php
+++ index.php
@@ -1,15 +1,2 @@
 <?php
-echo 'hhh';<<<<<<< HEAD
-echo 'hhh';
-=======
-/**
- * Created by PhpStorm.
- * User: טל
- * Date: 27/11/2017
- * Time: 23:26
- */
-
-echo '1';
-
 echo 'tal';
->>>>>>> 2db0bd029270c7732a725b16d90eb62bcde77290
