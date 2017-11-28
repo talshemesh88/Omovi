871f5b8e3efa5942205b0ec3cb3beaca62f41483
haim3307 <haim3307@live.com>
2017-11-28 22:26:20 +0200

    hht
    gh

--- index.php
+++ index.php
@@ -1,16 +1,2 @@
-/**** Created by Tal Shemesh on 27/11/2017.** All rights reserved Copying or selling this** code is a blatant copyright infringement** And will bring a lawsuit**/
-
 <?php
-/**
- * Created by PhpStorm.
- * User: טל
- * Date: 27/11/2017
- * Time: 23:26
- */
-
-<<<<<<< HEAD
-echo '1';
-=======
->>>>>>> 40b246f3474bbb2cbc381e2d31c1f71cb2471c6b
-
-echo 'tal';
+echo 'hhh';
